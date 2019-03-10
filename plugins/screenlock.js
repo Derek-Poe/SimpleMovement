08ef89c0e9a0fac5abf28975bf2ca8b0f7c71ede
Derek-P <48400713+Derek-P@users.noreply.github.com>
2019-03-11 00:35:27 +0900

    Add files via upload

---
+++ plugins/screenlock.js
@@ -0,0 +1,13 @@
+function lockReady() {
+
+	const { type } = screen.orientation;
+	console.log("Fullscreen and locked to ${type}. Ready!");
+
+}
+
+async function lockStart() {
+
+	await screen.orientation.lock("landscape-primary");
+	lockReady();
+
+}
